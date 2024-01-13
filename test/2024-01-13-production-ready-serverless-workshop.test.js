0150551dfc39d64fa7e581b009176e1a8bc46c07
Virginia Wu <virginiawu11@gmail.com>
2024-01-13 18:45:12 -0500

    Initial commit

---
+++ test/2024-01-13-production-ready-serverless-workshop.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const 20240113ProductionReadyServerlessWorkshop = require('../lib/2024-01-13-production-ready-serverless-workshop-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/2024-01-13-production-ready-serverless-workshop-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new 20240113ProductionReadyServerlessWorkshop.20240113ProductionReadyServerlessWorkshopStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
