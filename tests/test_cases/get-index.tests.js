a357511a8444ac1f1c5e6b426b1c77cd1416982b
Virginia Wu <virginiawu11@gmail.com>
2024-01-16 21:05:57 -0500

    add init prior to getting index.html

--- tests/test_cases/get-index.tests.js
+++ tests/test_cases/get-index.tests.js
@@ -1,7 +1,10 @@
+const { init } = require("../steps/init");
 const cheerio = require("cheerio");
 const when = require("../steps/when");
 
 describe(`When we invoke the GET / endpoint`, () => {
+  beforeAll(async () => await init());
+
   it(`Should return the index page with 8 restaurants`, async () => {
     const res = await when.we_invoke_get_index();
 
